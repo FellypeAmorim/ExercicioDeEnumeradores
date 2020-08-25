afb4dce5729bfdf2db9561e32479dc00107793a2
FellypeAmorim <fellype.amorim88@gmail.com>
2020-08-25 16:25:53 -0300

    Exercicio de Enumerador

---
+++ Exercicio Enumeradores/Entities/HourContract.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualBasic.CompilerServices;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercicio_Enumeradores.Entities
+{
+    class HourContract
+    {
+        public DateTime Date { get; set; }
+        public double ValuePerHour { get; set; }
+        public int Hours { get; set; }
+
+
+        HourContract()
+        {
+
+        }
+
+        public HourContract(DateTime date, double valuePerHour, int hours)
+        {
+            Date = date;
+            ValuePerHour = valuePerHour;
+            Hours = hours;
+        }
+
+        public double TotalValue()
+        {
+            return Hours * ValuePerHour;
+        }
+    }
+}
